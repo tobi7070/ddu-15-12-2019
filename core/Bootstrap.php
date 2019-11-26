23522ae718683ad3c32baf7d06344a6d160bb9fd
tobi7070 <54136828+tobi7070@users.noreply.github.com>
2019-11-26 09:48:30 +0100

    #1 #2 #3
    Initial setup

---
+++ core/Bootstrap.php
@@ -0,0 +1,34 @@
+<?php
+class Bootstrap
+{
+    public function __construct()
+    {
+        $path = isset($_GET['url']) ? $_GET['url'] : 'index.php';
+        // echo $path."<br>";
+        $path = strpos($path, ".") ? substr($path, 0, strpos($path, ".")) : $path;
+        // echo $path."<br>";
+        $path = trim($path, "/");
+        // echo $path."<br>";
+        $path = explode("/", $path);
+        // print_r($path);
+
+        $file = "controllers/". $path[0] . ".php";
+        if (file_exists($file)) {
+            require $file;
+        } else {
+	    echo "Error!";
+            return false;
+        }
+
+        $controller = new $path[0];
+
+        if (isset($path[2])) {
+            $controller->{$path[1]}($path[2]);
+        } elseif (isset($path[1])) {
+            $controller->{$path[1]}();
+        } else {
+            // defualt
+        }
+
+    }
+}
---
+++ index.php
@@ -0,0 +1,5 @@
+<?php
+require "core\Bootstrap.php";
+$app = new Bootstrap();
+
+// http://localhost/docs/index/action/param/.extension?query
